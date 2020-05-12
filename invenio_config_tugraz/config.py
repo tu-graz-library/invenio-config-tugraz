4bbf7ba242d7fab92ef5ab075bb0b408d1de79e6
mb <mb_wali@hotmail.com>
2020-05-12 14:36:37 +0200

    new configs

--- invenio_config_tugraz/config.py
+++ invenio_config_tugraz/config.py
@@ -16,3 +16,34 @@ INVENIO_CONFIG_TUGRAZ_DEFAULT_VALUE = 'foobar'
 
 INVENIO_CONFIG_TUGRAZ_BASE_TEMPLATE = 'invenio_config_tugraz/base.html'
 """Default base template for the demo page."""
+
+# Allowed Hosts
+APP_ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'invenio-dev01.tugraz.at', 'invenio-test.tugraz.at']
+
+# Allow the statics to build
+APP_DEFAULT_SECURE_HEADERS = {
+    'content_security_policy': {
+        'default-src': [
+            "'self'",
+            'fonts.googleapis.com',
+            '*.gstatic.com',
+            'data:',
+            "'unsafe-inline'",
+            "'unsafe-eval'",
+            "blob:",
+        ],
+    },
+    'content_security_policy_report_only': False,
+    'content_security_policy_report_uri': None,
+    'force_file_save': False,
+    'force_https': True,
+    'force_https_permanent': False,
+    'frame_options': 'sameorigin',
+    'frame_options_allow_from': None,
+    'session_cookie_http_only': True,
+    'session_cookie_secure': True,
+    'strict_transport_security': True,
+    'strict_transport_security_include_subdomains': True,
+    'strict_transport_security_max_age': 31556926,  # One year in seconds
+    'strict_transport_security_preload': False,
+}
