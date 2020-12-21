ec13d5abcf5cf727afc88bdf7f8621bf07a8b97c
mb <mb_wali@hotmail.com>
2020-12-21 14:03:44 +0100

    refactor: updating copyrights

--- invenio_config_tugraz/config.py
+++ invenio_config_tugraz/config.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2020 Mojib Wali.
+# Copyright (C) 2020 Graz University of Technology.
 #
 # invenio-config-tugraz is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
