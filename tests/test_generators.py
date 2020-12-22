ce65bde6770d7f88de0d0658489e16354701e851
mb <mb_wali@hotmail.com>
2020-12-22 15:35:05 +0100

    tests(generator): adding tests for RecordIp generator.

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -8,13 +8,21 @@
 
 """Test Generators."""
 
-# from invenio_config_tugraz.generators import RecordIp
+from invenio_access.permissions import any_user
+
+from invenio_config_tugraz.generators import RecordIp
 
 
-# def test_recordip():
-#     """Test Generator RecordIp."""
-#     generator = RecordIp()
+def test_recordip(create_app, open_record, singleip_record):
+    """Test Generator RecordIp."""
+    generator = RecordIp()
+    open_record = open_record
+    singleiprec = singleip_record
 
-#     assert generator.needs() == []
-#     assert generator.excludes() == []
-#     assert generator.query_filter().to_dict() == {"match_all": {}}
+    assert generator.needs(record=open_record) == [any_user]
+    assert generator.needs(record=singleiprec) == []
+
+    assert generator.excludes(record=open_record) == []
+    assert generator.excludes(record=open_record) == []
+
+    assert generator.query_filter().to_dict() == {'bool': {'must_not': [{'match': {'access.access_right': 'singleip'}}]}}
