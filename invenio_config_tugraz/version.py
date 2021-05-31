b33c7e09c363b26b487662c9db26ed3831f6e8b2
Mojib Wali <44528277+mb-wali@users.noreply.github.com>
2021-05-31 13:12:06 +0200

    v0.6.0

--- invenio_config_tugraz/version.py
+++ invenio_config_tugraz/version.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2020 Mojib Wali.
+# Copyright (C) 2020-2021 Graz University of Technology.
 #
 # invenio-config-tugraz is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -12,4 +12,4 @@ This file is imported by ``invenio_config_tugraz.__init__``,
 and parsed by ``setup.py``.
 """
 
-__version__ = "0.5.9"
+__version__ = "0.6.0"
