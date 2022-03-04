f0cbeb831ebae66ff85e77d4ee6100acbdace6b3
Mojib Wali <44528277+mb-wali@users.noreply.github.com>
2022-03-04 17:18:39 +0100

    Update config.py

--- invenio_config_tugraz/config.py
+++ invenio_config_tugraz/config.py
@@ -260,10 +260,15 @@ If the value is callable, its return value will be used for the field
 (e.g. lambda/function for dynamic calculation of values).
 """
 
-APP_RDM_DEPOSIT_FORM_AUTOCOMPLETE_NAMES = "search"
-"""Enable autocompleting names search field for creators/contributors.
+APP_RDM_DEPOSIT_FORM_AUTOCOMPLETE_NAMES = 'off'
+"""Behavior for autocomplete names search field for creators/contributors.
 
-(e.g. 'searc_only' or 'off')
+Available options:
+
+- ``search`` (default): Show search field and form always.
+- ``search_only``: Only show search field. Form displayed after selection or
+  explicit "manual" entry.
+- ``off``: Only show person form (no search field).
 """
 
 SQLALCHEMY_ECHO = False
