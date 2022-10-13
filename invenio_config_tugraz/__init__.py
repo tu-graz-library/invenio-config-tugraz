3fd16133f4ce12527ecb76f0953f561905c93d56
Mojib <mb_wali@hotmail.com>
2022-10-13 11:18:51 +0200

    release: v0.10.0

--- invenio_config_tugraz/__init__.py
+++ invenio_config_tugraz/__init__.py
@@ -11,6 +11,6 @@
 from .ext import InvenioConfigTugraz
 from .generators import RecordIp
 
-__version__ = "0.9.1"
+__version__ = "0.10.0"
 
 __all__ = ("__version__", "InvenioConfigTugraz", "RecordIp")
