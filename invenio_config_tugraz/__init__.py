eb38b1fcaaeca3a53d16ac815f1caac23b4867b7
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-05-24 20:10:03 +0200

    chore: apply ruff updates

--- invenio_config_tugraz/__init__.py
+++ invenio_config_tugraz/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2020-2024 Graz University of Technology.
+# Copyright (C) 2020-2025 Graz University of Technology.
 #
 # invenio-config-tugraz is free software; you can redistribute it and/or
 # modify it under the terms of the MIT License; see LICENSE file for more
@@ -14,7 +14,7 @@ from .utils import get_identity_from_user_by_email
 __version__ = "0.12.5"
 
 __all__ = (
-    "__version__",
     "InvenioConfigTugraz",
+    "__version__",
     "get_identity_from_user_by_email",
 )
