e9ac65045fba903ad6af7ac2fa6e5817ff93d158
Laura <lthaci@tugraz.at>
2026-09-16 15:12:16 +0200

    notices: add notice acknowledgment migration
    * create invenio config tugraz alembic branch
    * create notice acknowledgment table
    * register the invenio db alembic entry point

---
+++ invenio_config_tugraz/alembic/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2026 Graz University of Technology.
+#
+# invenio-config-tugraz is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see LICENSE file for more
+# details.
+
+"""Alembic migrations for invenio-config-tugraz."""
